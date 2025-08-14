e7dbeacda3758cea1815e0e6717a08e6e7877d92
eloizaboff <145134809+eloizaboff@users.noreply.github.com>
2025-08-14 11:49:35 -0300

    Update scritpt.js

---
+++ scritpt.js
@@ -0,0 +1,4 @@
+const caicaPrincipal = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document document.querySelector (".caica-resultado");
+const caixaResultado = document.querySelector(".texto-resultado");
