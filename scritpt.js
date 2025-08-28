920e3d5ff18d90b94285a734ddaaa1252425e184
eloizaboff <145134809+eloizaboff@users.noreply.github.com>
2025-08-28 11:50:38 -0300

    k

--- scritpt.js
+++ scritpt.js
@@ -9,19 +9,44 @@ enunciado:
 	"A inteligência artificial sempre decide sozinha ou depende 
 de como foi programada por humanos?"
 
-alternativa: "Sempre decide sozinha", "Depende de como foi programada",
+alternativas: "Sempre decide sozinha", "Depende de como foi programada",
 },
 {
 
     enunciado:
 "Quem cria e treina a inteligência artificial?"
 
-alternativa: "Ela mesma", "Programadores e cientistas de dados",
+alternativas: "Ela mesma", "Programadores e cientistas de dados",
 
 ],
 },
 
 {
+[
+    enunciado:
+     A IA pode aprender com os erros e melhorar?
 
-    enunciado: A IA pode aprender com os erros e melhorar?
-a) Sim
+alternativas: "sim" , "nao"],
+}
+
+    {
+[enunciado:
+"Quando você conversa com uma IA, quem define
+ o rumo da conversa?"
+
+alternativas:"Só a máquina" , "Você, com suas perguntas e escolhas"
+],
+}
+{
+
+[enunciado:
+"A inteligência artificial pode ter sentimentos iguais aos
+ humanos?"
+alternativas:"Sim" , "não"
+],
+}
+
+
+
+
+
