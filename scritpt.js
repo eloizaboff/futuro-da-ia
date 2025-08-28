a5038826bf4328ca1443f2f82b03c8d372a51e9d
eloizaboff <145134809+eloizaboff@users.noreply.github.com>
2025-08-28 11:35:58 -0300

    m

--- scritpt.js
+++ scritpt.js
@@ -2,3 +2,26 @@ const caicaPrincipal = document.querySelector(".caixa-principal");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document document.querySelector (".caica-resultado");
 const caixaResultado = document.querySelector(".texto-resultado");
+
+const pergunta = [
+{
+enunciado:
+	"A inteligência artificial sempre decide sozinha ou depende 
+de como foi programada por humanos?"
+
+alternativa: "Sempre decide sozinha", "Depende de como foi programada",
+},
+{
+
+    enunciado:
+"Quem cria e treina a inteligência artificial?"
+
+alternativa: "Ela mesma", "Programadores e cientistas de dados",
+
+],
+},
+
+{
+
+    enunciado: A IA pode aprender com os erros e melhorar?
+a) Sim
