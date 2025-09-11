f9773d89f4046a3b08944b3b4649ec6901d472b7
eloizaboff <145134809+eloizaboff@users.noreply.github.com>
2025-09-11 11:50:06 -0300

    ..

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const caicaPrincipal = document.querySelector(".caixa-principal");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document document.querySelector (".caica-resultado");
-const caixaResultado = document.querySelector(".texto-resultado");
+const caixaResultado = document document.querySelector (".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const pergunta = [
+const perguntas = [
 {
 enunciado:
 	"A inteligência artificial sempre decide sozinha ou depende 
